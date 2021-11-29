f0f37608f178d506091db6eecae29e10de292b66
John <jddhalogamer@gmail.com>
2021-11-29 15:22:38 -0600

    app now connects to local db.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const mongoose = require('mongoose')
 
 
 const app = express()
@@ -12,6 +17,13 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+
+db.on('error', (err) => console.error(err))
+db.once('open', () => console.log('Connected to Mongoose.'))
+
 app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 5000)
