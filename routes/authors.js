d8154b3fa804d843a0def7ecacab3e2639921f65
John <jddhalogamer@gmail.com>
2021-11-30 15:53:21 -0600

    refactored POST into a try/catch block for cleaner code.

--- routes/authors.js
+++ routes/authors.js
@@ -13,22 +13,21 @@ router.get('/new', (req, res) => {
 })
 
 //Create Author Route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
     })
-    author.save((err, newAuthor) => {
-        if (err) {
-            let locals = { errorMessage: 'Error creating Author'}
-            res.render('authors/new', {
-                author: author,
-                locals: locals
-            })
-        } else {
-            // res.redirect(`authors/${newAuthor.id}`)
-            res.redirect('authors')
-        }
-    })
+    try {
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    } catch {
+        let locals = { errorMessage: 'Error creating Author'}
+        res.render('authors/new', {
+            author: author,
+            locals: locals
+        })
+    }
 })
 
 module.exports = router
